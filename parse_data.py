5466a287380880ed822245dab15bf736d37bbb8a
Siddharth Sahay <sahaysid@gmail.com>
2017-07-06 15:35:01 +0530

    Parsing

--- parse_data.py
+++ parse_data.py
@@ -1,13 +1,19 @@
 
 data_file = "accounting_log_2009_icl.sdata"
 
+field_list = ["ctime", "qtime", "etime", "start", "resources_used.walltime", "resources_used.vmem", "resources_used.ncpus",\
+		"resources_used.mem", "resources_used.cpupercent", "resources_used.cput", "Resource_List.walltime", "Resource_List.select"]
+
+headers = field_list[:-1] + ["Resource_List.mem", "Resource_List.ncpus"]
+
+
 def readLine(line):
 	# format is <time;record-type;id;string>
 	# time is irrelevant ?
 	parsed = []
 	if line[1] == "E":
 		parsed.append(line[2]) #id
-		return line[3].split(" ")
+		#parse line and return only header values
 	else:
 		return None
 
@@ -16,13 +22,12 @@ def parse(content):
 	for line in content:
 		line = line.strip()
 		parsedLine = readLine(line.split(";"))
-		print(parsedLine)
 		if parsedLine is not None:
 			data_l.append(parsedLine)
-	print(data_l)
+			data_l.append(len(parsedLine))
+	print(data_l[450])
 
 if __name__=="__main__":
 	with open(data_file, "r") as datafp:
 		content = [line.rstrip('\n') for line in datafp]
 		data_l = parse(content)
-		print data_l
