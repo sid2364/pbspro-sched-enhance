e2a593b35363e1990d6cdc0f5f726ad1c0cc3557
Siddharth Sahay <sahaysid@gmail.com>
2017-08-09 17:38:41 +0530

    Conversions for time, memory, nums

--- parse_data.py
+++ parse_data.py
@@ -7,6 +7,22 @@ field_list = ["ctime", "qtime", "etime", "start", "resources_used.walltime", "re
 
 headers = field_list[:-1] + ["Resource_List.mem", "Resource_List.ncpus"]
 
+time_fields = ["Resource_List.walltime", "resources_used.cput", "resources_used.walltime"]
+number_fields = ["session", "etime", "start", "Resource_List.ncpus", "end", "ctime", "qtime",\
+			"resources_used.ncpus", "resources_used.ncpus", "Resource_List.nodect"]
+mem_fields = ["Resource_List.mem", "resources_used.mem", "resources_used.vmem"]
+
+def convertToGMKb(memory_string, target='kb'):
+	gmk_d = {'kb': 1, 'mb': 2, 'gb': 3}
+	gmk = memory_string[-2:].lower()
+	size = float(memory_string[:-2])
+	shift = gmk_d[target] - gmk_d[gmk]
+	if shift != 0:
+		size /= 1024**shift
+	return size
+
+def convertTimeToSeconds(time_string, ftr=[3600,60,1]):
+	return sum([a*b for a,b in zip(ftr, map(int,time_string.split(':')))])
 
 def readLine(line):
 	# format is <time;record-type;id;string>
@@ -20,7 +36,15 @@ def readLine(line):
 		for part in line[3].split(" "):
 			field_name = part.split('=')[0]
 			field_value = ''.join(part.split('=')[1:])
-			lineta[field_name] = field_value
+			if field_name in time_fields:
+				field_value_i = convertTimeToSeconds(field_value)
+			elif field_name in number_fields:
+				field_value_i = int(field_value)
+			elif field_name in mem_fields:
+				field_value_i = convertToGMKb(field_value)
+			else:
+				field_value_i = field_value
+			lineta[field_name] = field_value_i
 			count += 1
 		lineta['arg_count'] = count
 	else:
@@ -35,10 +59,11 @@ def parse(content):
 		parsedLine = readLine(line.split(";"))
 		if parsedLine is not None:
 			data_l.append(parsedLine)
-	#print(json.dumps(data_l, indent=4))
+	print(json.dumps(data_l, indent=4))
 	return data_l
 
 if __name__=="__main__":
 	with open(data_file, "r") as datafp:
 		content = [line.rstrip('\n') for line in datafp]
 		data_l = parse(content)
+	
