30395706b31d59041f8677ab7dc91a4b6a72c2f3
Siddharth Sahay <sahaysid@gmail.com>
2017-08-13 13:42:47 +0530

    Testing classifiers

--- train.py
+++ train.py
@@ -8,7 +8,12 @@ features_data, labels_data = seg.split_features(data)
 features_train, features_test = seg.segment_data(features_data)
 labels_train, labels_test = seg.segment_data(labels_data)
 
-classifier = ensemble.RandomForestClassifier(n_estimators=500,max_features=3,min_samples_split=5,oob_score=True)
+#classifier = ensemble.RandomForestClassifier(n_estimators=500,max_features=3,min_samples_split=5,oob_score=True)
+classifier = svm.SVC(kernel='linear', cache_size=20, verbose=True)
+#classifier = neighbors.KNeighborsClassifier(algorithm="auto", weights="distance", n_neighbors=15)
+#classifier = ensemble.AdaBoostClassifier(n_estimators=50, learning_rate=0.5)
+
+print("Going to fit " + str(len(features_train)) + " training rows with " + str(len(labels_train)) + " labels.")
 
 print("Training...")
 classifier.fit(features_train, labels_train)
