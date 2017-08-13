72e4bf3708940dd2fc21886f8cf8a50930d130bc
Siddharth Sahay <sahaysid@gmail.com>
2017-08-13 15:01:18 +0530

    Plotter

---
+++ plot.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import parse_data as parse
+import segment as seg
+
+train_ft=["Resource_List.walltime"]
+
+target_ft="resources_used.walltime"
+
+data = parse.getData()
+RL_walltime, RU_walltime = seg.split_features(data, train_ft, target_ft)
+
+print(len(RL_walltime), len(RU_walltime))
+
+plt.plot(RL_walltime, RU_walltime)
+plt.show()
